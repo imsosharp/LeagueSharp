5e5d4cbfd1def3db60bc9c0cff9278422c5788ef
imsosharp <imsosharp@mail.com>
2014-12-07 17:06:04 +0200

    wot

--- QuickSimpleYetSoSharpPositionRevealer/QuickSimpleYetSoSharpPositionRevealer/Program.cs
+++ QuickSimpleYetSoSharpPositionRevealer/QuickSimpleYetSoSharpPositionRevealer/Program.cs
@@ -23,7 +23,7 @@ namespace QuickSimpleYetSoSharpPositionRevealer
                         LastY = myHero.Position.Y;
                         LastZ = myHero.Position.Z;
                         INODIWPY = "Your champion's current position is X: " + LastX.ToString("R") + " Y: " + LastY.ToString("R") + " Z: " + LastZ.ToString("R");
-                        Game.PrintChat("Displaced");
+                        Game.PrintChat(INODIWPY);
 
                         
                     }

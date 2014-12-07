7ff029727ec6e9daa1618d5184966949bf070e66
imsosharp <imsosharp@mail.com>
2014-12-07 14:50:29 +0200

    zzzzz

--- QuickSimpleYetSoSharpPositionRevealer/QuickSimpleYetSoSharpPositionRevealer/Program.cs
+++ QuickSimpleYetSoSharpPositionRevealer/QuickSimpleYetSoSharpPositionRevealer/Program.cs
@@ -11,6 +11,7 @@ namespace QuickSimpleYetSoSharpPositionRevealer
     class Program
     {
         private static Obj_AI_Hero myHero = ObjectManager.Player;
+        private static float LastX, LastY, LastZ = 0;
 
         static void Main(string[] args)
         {
@@ -18,14 +19,19 @@ namespace QuickSimpleYetSoSharpPositionRevealer
         }
         private static void OnGameLoad(EventArgs args)
         {
-            spamPos();
+            printPos();
         }
-        private static void spamPos()
+        private static void printPos()
         {
-            while(true)
+            while (true)
             {
-                Game.PrintChat("Your champion's current position is X: {0} Y: {1} Z: {2}", myHero.Position.X.ToString(), myHero.Position.Y.ToString(), myHero.Position.Z.ToString());
-                System.Threading.Thread.Sleep(1500);
+                if (!(LastX == myHero.Position.X) && !(LastY == myHero.Position.Y) && !(LastZ == myHero.Position.Z))
+                {
+                    Game.PrintChat("Your champion's current position is X: {0} Y: {1} Z: {2}", myHero.Position.X.ToString(), myHero.Position.Y.ToString(), myHero.Position.Z.ToString());
+                    LastX = myHero.Position.X;
+                    LastY = myHero.Position.Y;
+                    LastZ = myHero.Position.Z;
+                }
             }
         }
     }

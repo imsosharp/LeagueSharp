5e1fba3821fdf73ebd96eb464f24d89f400cb0a7
imsosharp <imsosharp@mail.com>
2014-12-07 16:27:35 +0200

    c

--- QuickSimpleYetSoSharpPositionRevealer/QuickSimpleYetSoSharpPositionRevealer/Program.cs
+++ QuickSimpleYetSoSharpPositionRevealer/QuickSimpleYetSoSharpPositionRevealer/Program.cs
@@ -16,15 +16,16 @@ namespace QuickSimpleYetSoSharpPositionRevealer
 
         static void Main(string[] args)
         {
-            while (myHero.Level >= 1)
+            while (true)
             {
                 if (myHero.Position.X != null && myHero.Position.Y != null && myHero.Position.Z != null)
                 {
                     Game.PrintChat("myHero.Position not null");
                     if (!(LastX == myHero.Position.X) && !(LastY == myHero.Position.Y) && !(LastZ == myHero.Position.Z))
                     {
-                        INODIWPY = "Your champion's current position is X: " + myHero.Position.X.ToString() + " Y: " + myHero.Position.Y.ToString() + " Z: " + myHero.Position.Z.ToString();
+                        /* INODIWPY = "Your champion's current position is X: " + myHero.Position.X.ToString() + " Y: " + myHero.Position.Y.ToString() + " Z: " + myHero.Position.Z.ToString();
                         Game.PrintChat(INODIWPY);
+                         */
 
                         LastX = myHero.Position.X;
                         LastY = myHero.Position.Y;

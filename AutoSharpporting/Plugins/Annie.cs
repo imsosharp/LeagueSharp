b7f0842557188d14d7d81cfa263d437f0003e24b
imsosharp <imsosharp@mail.com>
2014-12-07 20:40:17 +0200

    Annie

---
+++ AutoSharpporting/Plugins/Annie.cs
@@ -0,0 +1,82 @@
+﻿#region LICENSE
+
+// Copyright 2014 - 2014 Support
+// Braum.cs is part of Support.
+// Support is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// Support is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with Support. If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+
+#region
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+using Support.Evade;
+using Support.Util;
+using ActiveGapcloser = Support.Util.ActiveGapcloser;
+using SpellData = LeagueSharp.SpellData;
+
+#endregion
+
+namespace Support.Plugins
+{
+    public class Annie : PluginBase
+    {
+        public Annie()
+        {
+            Q = new Spell(SpellSlot.Q, 650);
+            W = new Spell(SpellSlot.W, 625);
+            E = new Spell(SpellSlot.E);
+            R = new Spell(SpellSlot.R, 600);
+
+            Q.SetTargetted(250, 1400);
+            W.SetSkillshot(600, (float)(50 * Math.PI / 180), float.MaxValue, false, SkillshotType.SkillshotCone);
+            R.SetSkillshot(250, 200, float.MaxValue, false, SkillshotType.SkillshotCircle);
+        }
+
+
+
+
+
+
+        public override void OnUpdate(EventArgs args)
+        {
+            if (ComboMode)
+            {
+                if (Q.CastCheck(Target, "Combo.Q"))
+                {
+                    Q.Cast(Target, false);
+                }
+                if (W.CastCheck(Target, "Combo.W"))
+                {
+                    W.Cast(Target, true);
+                }
+
+                if (R.CastCheck(Target, "Combo.R"))
+                {
+                    R.Cast(Target, true);
+                }
+            }
+        }
+
+
+        public override void ComboMenu(Menu config)
+        {
+            config.AddBool("Combo.Q", "Use Q", true);
+            config.AddBool("Combo.W", "Use W", true);
+            config.AddBool("Combo.R", "Use R", true);
+        }
+    }
+}

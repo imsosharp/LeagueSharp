4ed275c4fce100293fd33cf4514b41ba16c23d2b
imsosharp <imsosharp@mail.com>
2014-12-07 22:07:00 +0200

    temp shopping & levelup

--- AutoSharpporting/Autoplay.cs
+++ AutoSharpporting/Autoplay.cs
@@ -23,6 +23,9 @@ namespace Support
         private static Vector3 lanepos;
         private static Vector3 orbwalkingpos1;
         private static Vector3 orbwalkingpos2;
+        static int qlvl, wlvl, elvl, rlvl, buyIndex = 0;
+        static int[] abilityOrder = { 1, 2, 3, 2, 2, 4, 2, 1, 2, 1, 4, 1, 1, 3, 3, 4, 3, 3, }; //spell level order
+        static int[] shopList = { 3047, 1011, 3134, 3068, 3024, 3025, 3071, 3082, 3143, 3005, 0 }; //list of items to buy
         
 
         public Autoplay()
@@ -35,6 +38,35 @@ namespace Support
         }
         private static void OnUpdate(EventArgs args)
         {
+            if (bot.Level == 1)
+            {
+                SpellSlot abilitySlot;
+                if (abilityOrder[0] == 1)
+                {
+                    abilitySlot = SpellSlot.Q;
+                    qlvl++;
+                }
+                else if (abilityOrder[0] == 2)
+                {
+                    abilitySlot = SpellSlot.W;
+                    wlvl++;
+                }
+                else if (abilityOrder[0] == 3)
+                {
+                    abilitySlot = SpellSlot.E;
+                    elvl++;
+                }
+                else if (abilityOrder[0] == 4)
+                {
+                    abilitySlot = SpellSlot.R;
+                    rlvl++;
+                }
+                else
+                {
+                    abilitySlot = SpellSlot.Q;
+                }
+                ObjectManager.Player.Spellbook.LevelUpSpell(abilitySlot);
+            }
             doAutoplay();
         }
         public void OnSendPacket(GamePacketEventArgs args)
@@ -47,7 +79,36 @@ namespace Support
         }
         public void OnLevelUp(Obj_AI_Base sender, CustomEvents.Unit.OnLevelUpEventArgs args)
         {
-
+            if ((qlvl + wlvl + elvl + rlvl) < bot.Level)
+            {
+                int i = bot.Level - 1;
+                SpellSlot abilitySlot;
+                if (abilityOrder[i] == 1)
+                {
+                    abilitySlot = SpellSlot.Q;
+                    qlvl++;
+                }
+                else if (abilityOrder[i] == 2)
+                {
+                    abilitySlot = SpellSlot.W;
+                    wlvl++;
+                }
+                else if (abilityOrder[i] == 3)
+                {
+                    abilitySlot = SpellSlot.E;
+                    elvl++;
+                }
+                else if (abilityOrder[i] == 4)
+                {
+                    abilitySlot = SpellSlot.R;
+                    rlvl++;
+                }
+                else
+                {
+                    abilitySlot = SpellSlot.Q;
+                }
+                ObjectManager.Player.Spellbook.LevelUpSpell(abilitySlot);
+            }
         }
         public void OnGameEnd(EventArgs args)
         {
@@ -79,8 +140,31 @@ namespace Support
                 }
             }
 
+            if (Utility.InFountain())
+            {
+                if (shopList[buyIndex] != 0)
+                {
+                    int thisItem = shopList[buyIndex];
+                    if (!hasItem(thisItem))
+                    {
+                        buyItem(thisItem);
+                    }
+                    if (hasItem(thisItem))
+                    {
+                        buyIndex += 1;
+                    }
+                }
+            }
+        }
+            public static bool hasItem(int id)
+            {
+                return Items.HasItem(id, bot);
+            }
+            public static void buyItem(int id)
+            {
+                Packet.C2S.BuyItem.Encoded(new Packet.C2S.BuyItem.Struct(id, ObjectManager.Player.NetworkId)).Send();
+            }
+
         }
 
     }
-    
-}

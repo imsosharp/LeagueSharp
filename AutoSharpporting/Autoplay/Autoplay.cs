ee4d83eddc32ff3df68688f16ff218b7af9921d6
imsosharp <imsosharp@mail.com>
2014-12-30 14:42:41 +0200

    oups

--- AutoSharpporting/Autoplay/Autoplay.cs
+++ AutoSharpporting/Autoplay/Autoplay.cs
@@ -82,7 +82,7 @@ namespace Support
 
         private static bool IsBotSafe()
         {
-            if (Bot.InFountain() && !((Bot.Health / Bot.MaxHealth) * 100 < 90))
+            if (Bot.InFountain() && !((Bot.Health / Bot.MaxHealth) * 100 > 90))
             {
                 return false;
             }

538a4ceb8f13640481eddeb38459f73be96f4067
imsosharp <imsosharp@mail.com>
2014-12-30 14:47:34 +0200

    c

--- AutoSharpporting/Properties/AssemblyInfo.cs
+++ AutoSharpporting/Properties/AssemblyInfo.cs
@@ -56,5 +56,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("4.21.6.7")]
-[assembly: AssemblyFileVersion("4.21.6.7")]
+[assembly: AssemblyVersion("4.21.7.9")]
+[assembly: AssemblyFileVersion("4.21.7.9")]

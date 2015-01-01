0b2f2097e990df113fed715b4b46081d15210939
imsosharp <imsosharp@mail.com>
2015-01-01 04:27:46 +0200

    oups

--- AutoSharpporting BETA/Autoplay/Autoplay.cs
+++ AutoSharpporting BETA/Autoplay/Autoplay.cs
@@ -11,7 +11,6 @@ using System;
 using System.Collections.Generic;
 using System.Drawing.Text;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using LeagueSharp;
 using LeagueSharp.Common;
 using SharpDX;

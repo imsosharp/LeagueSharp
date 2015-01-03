de7db23c439e4ba86793824c3f4e6d56caa15ffd
imsosharp <imsosharp@mail.com>
2015-01-03 14:49:27 +0200

    :+1:

--- AutoSharpporting BETA/Autoplay/FileHandler.cs
+++ AutoSharpporting BETA/Autoplay/FileHandler.cs
@@ -11,16 +11,22 @@ namespace Support
 {
     internal class FileHandler
     {
-        private static string _cBuildsPath = "%appdata%\\AutoSharpporting\\";
-        private static string theFile = _cBuildsPath + Autoplay.Bot.BaseSkinName + ".txt";
+        private static string _cBuildsPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\AutoSharpporting\\"; //y u do dis csharp
+        private static string theFile = ""; //better to intialize at ongameload
         private static string[] _itemsStringArray = { };
         public static int[] Items = { };
 
         public static void DoChecks()
         {
+            theFile = _cBuildsPath + Utility.Map.GetMap().Type.ToString() + Autoplay.Bot.BaseSkinName + ".txt";
             if (!Directory.Exists(_cBuildsPath))
             {
                 Directory.CreateDirectory(_cBuildsPath);
+                Directory.CreateDirectory(_cBuildsPath + Utility.Map.MapType.CrystalScar.ToString());
+                Directory.CreateDirectory(_cBuildsPath + Utility.Map.MapType.HowlingAbyss.ToString());
+                Directory.CreateDirectory(_cBuildsPath + Utility.Map.MapType.SummonersRift.ToString());
+                Directory.CreateDirectory(_cBuildsPath + Utility.Map.MapType.TwistedTreeline.ToString());
+                Directory.CreateDirectory(_cBuildsPath + Utility.Map.MapType.Unknown.ToString());
             }
             if (ExistsCustomBuild())
             {

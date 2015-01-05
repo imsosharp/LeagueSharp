94b5ae43e99ead9ff3f093deb92ae64c96180475
nongnoobjung <kitcharuk_4@hotmail.com>
2015-01-06 01:10:26 +0700

    more plugin forbot
    more plugin forbot

---
+++ AutoSharpporting/Plugins/Ashe.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+using Support.Evade;
+using Support.Util;
+using ActiveGapcloser = Support.Util.ActiveGapcloser;
+using SpellData = LeagueSharp.SpellData;
+
+namespace Support.Plugins
+{
+    public class Ashe : PluginBase
+    {
+        public Ashe()
+        {
+            Q = new Spell(SpellSlot.Q);
+            W = new Spell(SpellSlot.W, 1200);
+            E = new Spell(SpellSlot.E, 2500);
+            R = new Spell(SpellSlot.R, 20000);
+
+
+            W.SetSkillshot(250f, (float)(24.32f * Math.PI / 180), 902f, true, SkillshotType.SkillshotCone);
+            E.SetSkillshot(377f, 299f, 1400f, false, SkillshotType.SkillshotLine);
+            R.SetSkillshot(250f, 130f, 1600f, false, SkillshotType.SkillshotLine);
+        }
+
+        public override void OnUpdate(EventArgs args)
+        {
+            if (ComboMode)
+            {
+                if (!IsQActive)
+                {
+                    Q.Cast();
+                }
+                if (W.CastCheck(Target, "ComboW"))
+                {
+                    W.Cast(Target, true);
+                }
+
+                if (R.CastCheck(Target, "ComboR"))
+                {
+                    R.Cast(Target, true);
+                }
+            }
+
+            if (HarassMode)
+            {
+                if (W.CastCheck(Target, "HarassW"))
+                {
+                    W.Cast(Target, true);
+                }
+            }
+        }
+
+        public override void OnPossibleToInterrupt(Obj_AI_Base unit, InterruptableSpell spell)
+        {
+            if (spell.DangerLevel < InterruptableDangerLevel.High || unit.IsAlly)
+            {
+                return;
+            }
+
+            if (R.CastCheck(unit, "Interrupt.R"))
+            {
+                R.Cast(unit, UsePackets);
+                return;
+            }
+        
+        }
+
+        public bool IsQActive
+        {
+            get
+            {
+                return ObjectManager.Player.HasBuff("FrostShot");
+            }
+        }
+
+        public override void ComboMenu(Menu config)
+        {
+            config.AddBool("ComboQ", "Use Q", true);
+            config.AddBool("ComboW", "Use W", true);
+            config.AddBool("ComboR", "Use R", true);
+        }
+
+        public override void HarassMenu(Menu config)
+        {
+            config.AddBool("HarassW", "Use W", true);
+        }
+
+        public override void InterruptMenu(Menu config)
+        {
+            config.AddBool("Interrupt.R", "Use R to Interrupt Spells", true);
+        }
+    }
+}
---
+++ AutoSharpporting/Plugins/Veigar.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+using Support.Evade;
+using Support.Util;
+using ActiveGapcloser = Support.Util.ActiveGapcloser;
+using SpellData = LeagueSharp.SpellData;
+
+namespace Support.Plugins
+{
+    public class Veigar : PluginBase
+    {
+        public Veigar()
+        {
+
+            Q = new Spell(SpellSlot.Q, 650);
+            W = new Spell(SpellSlot.W, 900);
+            E = new Spell(SpellSlot.E, 1005);
+            R = new Spell(SpellSlot.R, 650);
+            W.SetSkillshot(1.25f, 230f, float.MaxValue, false, SkillshotType.SkillshotCircle);
+            E.SetSkillshot(.2f, 330f, float.MaxValue, false, SkillshotType.SkillshotCircle);
+
+        }
+
+
+        public override void OnUpdate(EventArgs args)
+        {
+            if (ComboMode)
+            {
+                if (E.CastCheck(Target, "ComboE"))
+                {
+                    castE((Obj_AI_Hero)Target);
+                }
+                if (Q.CastCheck(Target, "ComboQ"))
+                {
+                    Q.Cast(Target, true);
+                }
+                if (W.CastCheck(Target, "ComboW"))
+                {
+                    W.Cast(Target, true);
+                }
+                if (R.CastCheck(Target, "ComboR"))
+                {
+                    R.Cast(Target, true);
+                }
+            }
+
+            if (HarassMode)
+            {
+                if (Q.CastCheck(Target, "HarassQ"))
+                {
+                    Q.Cast(Target, true);
+                }
+                if (W.CastCheck(Target, "HarassW"))
+                {
+                    W.Cast(Target, true);
+                }
+            }
+        }
+
+        public override void OnPossibleToInterrupt(Obj_AI_Base unit, InterruptableSpell spell)
+        {
+            if (spell.DangerLevel < InterruptableDangerLevel.High || unit.IsAlly)
+            {
+                return;
+            }
+
+            if (E.CastCheck(unit, "Interrupt.E"))
+            {
+                castE((Obj_AI_Hero)unit);
+                return;
+            }
+        }
+
+        public override void ComboMenu(Menu config)
+        {
+            config.AddBool("ComboQ", "Use Q", true);
+            config.AddBool("ComboQ", "Use W", true);
+            config.AddBool("ComboE", "Use E", true);
+            config.AddBool("ComboR", "Use R", true);
+        }
+
+        public override void HarassMenu(Menu config)
+        {
+            config.AddBool("HarassQ", "Use Q", true);
+            config.AddBool("HarassW", "Use W", true);
+        }
+
+        public override void InterruptMenu(Menu config)
+        {
+            config.AddBool("Interrupt.E", "Use E to Interrupt Spells", true);
+        }
+
+        public override void castE(Obj_AI_Base target)
+        {
+            PredictionOutput pred = Prediction.GetPrediction(target,E.Delay);
+            Vector2 castVec = pred.UnitPosition.To2D() -
+                              Vector2.Normalize(pred.UnitPosition.To2D() - Player.Position.To2D()) * E.Width;
+
+            if (pred.Hitchance >= HitChance.High && E.IsReady())
+            {
+                E.Cast(castVec);
+            }
+        }
+
+        /// Cast E Credit DedToto
+        //E CAST(UNIT)
+        public override void castE(Obj_AI_Hero target)
+        {
+            PredictionOutput pred = Prediction.GetPrediction(target, E.Delay);
+            Vector2 castVec = pred.UnitPosition.To2D() -
+                              Vector2.Normalize(pred.UnitPosition.To2D() - Player.Position.To2D()) * E.Width;
+
+            if (pred.Hitchance >= HitChance.High && E.IsReady())
+            {
+                E.Cast(castVec, UsePackets);
+            }
+        }
+
+        //E CAST(COORDINATES)
+        public override void castE(Vector3 pos)
+        {
+            Vector2 castVec = pos.To2D() -
+                              Vector2.Normalize(pos.To2D() - Player.Position.To2D()) * E.Width;
+
+            if (E.IsReady())
+            {
+                E.Cast(castVec, UsePackets);
+            }
+        }
+    }
+}
+
+

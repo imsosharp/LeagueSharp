74abdef98962362e71bf4f932218bc5b83bb7e54
imsosharp <imsosharp@mail.com>
2015-01-11 16:18:45 +0200

    NiceGuySharp

---
+++ NiceGuySharp/NiceGuySharp/FileHandler.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Reflection;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace NiceGuySharp
+{
+    internal class FileHandler
+    {
+        public static string Folder = Config.LeagueSharpDirectory + @"\NiceGuySharp\";
+
+
+        public static string OnGameStartTxt;
+        public static string OnDeathTxt;
+        public static string OnGameEndTxt;
+
+        public static string OnKillTxt;
+        public static string OnDoubleTxt;
+        public static string OnTripleTxt;
+        public static string OnQuadraTxt;
+        public static string OnPentaTxt;
+
+        public static string OnAllyKillTxt;
+        public static string OnAllyDoubleTxt;
+        public static string OnAllyTripleTxt;
+        public static string OnAllyQuadraTxt;
+        public static string OnAllyPentaTxt;
+
+        public static string OnEnemyKillTxt;
+        public static string OnEnemyDoubleTxt;
+        public static string OnEnemyTripleTxt;
+        public static string OnEnemyQuadraTxt;
+        public static string OnEnemyPentaTxt;
+
+        public static string[] ParseFile(Type file)
+        {
+            if (file != null)
+            {
+                
+            }
+            return new string[1];
+        }
+
+        public static void DoChecks()
+        {
+            OnGameStartTxt = Folder + "OnGameStart.txt";
+            OnGameEndTxt = Folder + "OnGameEnd.txt";
+
+            OnKillTxt = Folder + "OnKill.txt";
+            OnDeathTxt = Folder + "OnDeath.txt";
+            OnDoubleTxt = Folder + "OnDouble.txt";
+            OnTripleTxt = Folder + "OnTriple.txt";
+            OnQuadraTxt = Folder + "OnQuadra.txt";
+            OnPentaTxt = Folder + "OnPenta.txt";
+
+            OnAllyKillTxt = Folder + "OnAllyKill.txt";
+            OnAllyDoubleTxt = Folder + "OnAllyDouble.txt";
+            OnAllyTripleTxt = Folder + "OnAllyTriple.txt";
+            OnAllyQuadraTxt = Folder + "OnAllyQuadra.txt";
+            OnAllyPentaTxt = Folder + "OnAllyPenta.txt";
+
+            OnEnemyKillTxt = Folder + "OnEnemyKill.txt";
+            OnEnemyDoubleTxt = Folder + "OnEnemyDouble.txt";
+            OnEnemyTripleTxt = Folder + "OnEnemyTriple.txt";
+            OnEnemyQuadraTxt = Folder + "OnEnemyQuadra.txt";
+            OnEnemyPentaTxt = Folder + "OnEnemyPenta.txt";
+
+            if (!Directory.Exists(Folder))
+            {
+                Directory.CreateDirectory(Folder);
+            }
+            if (!File.Exists(OnGameStartTxt))
+            {
+                var newfile = File.Create(OnGameStartTxt);
+                newfile.Close();
+                var content = "glhf\ngl hf\nhi";
+                var separator = new string[] { "\n" };
+                string[] lines = content.Split(separator, StringSplitOptions.None);
+                File.WriteAllLines(OnGameStartTxt, lines);
+            }
+            if (!File.Exists(OnDeathTxt))
+            {
+                var newfile = File.Create(OnDeathTxt);
+                newfile.Close();
+                var content = "oups\nwot\nwtf\ndat dmg\nthat damage\nfug\nfugg :DDD\nfml\ndamn I suck\nsorry";
+                var separator = new string[] { "\n" };
+                string[] lines = content.Split(separator, StringSplitOptions.None);
+                File.WriteAllLines(OnDeathTxt, lines);
+            }
+            if (!File.Exists(OnGameEndTxt))
+            {
+                var newfile = File.Create(OnGameEndTxt);
+                newfile.Close();
+                var content = "oups\nwot\nwtf\ndat dmg\nthat damage\nfug\nfugg :DDD\nfml\ndamn I suck\nsorry";
+                var separator = new string[] { "\n" };
+                string[] lines = content.Split(separator, StringSplitOptions.None);
+                File.WriteAllLines(OnGameEndTxt, lines);
+            }
+            if (!File.Exists(OnKillTxt))
+            {
+                var newfile = File.Create(OnKillTxt);
+                newfile.Close();
+                var content = "yay\nfk yeah\nowned\npwned\nkicked dat booty\nowned that ass\npwned that noob";
+                var separator = new string[] { "\n" };
+                string[] lines = content.Split(separator, StringSplitOptions.None);
+                File.WriteAllLines(OnKillTxt, lines);
+            }
+            if (!File.Exists(OnDoubleTxt))
+            {
+                var newfile = File.Create(OnDoubleTxt);
+                newfile.Close();
+                var content = "yay\nfk yeah\nowned\npwned\nright in the balls";
+                var separator = new string[] { "\n" };
+                string[] lines = content.Split(separator, StringSplitOptions.None);
+                File.WriteAllLines(OnDoubleTxt, lines);
+            }
+            if (!File.Exists(OnTripleTxt))
+            {
+                var newfile = File.Create(OnTripleTxt);
+                newfile.Close();
+                var content = "oooh baby a triple!\ni = god";
+                var separator = new string[] { "\n" };
+                string[] lines = content.Split(separator, StringSplitOptions.None);
+                File.WriteAllLines(OnTripleTxt, lines);
+            }
+            if (!File.Exists(OnQuadraTxt))
+            {
+                var newfile = File.Create(OnQuadraTxt);
+                newfile.Close();
+                var content = "godlike\nhaha\nowned\nxD";
+                var separator = new string[] { "\n" };
+                string[] lines = content.Split(separator, StringSplitOptions.None);
+                File.WriteAllLines(OnQuadraTxt, lines);
+            }
+            if (!File.Exists(OnPentaTxt))
+            {
+                var newfile = File.Create(OnPentaTxt);
+                newfile.Close();
+                var content = "PENTAKIRRU\nNULLED\nMe > Ur Whole Team\nLel REKT\nSo REKT\nREKT n ROLL baby\nomg I did it mom\nokay";
+                var separator = new string[] { "\n" };
+                string[] lines = content.Split(separator, StringSplitOptions.None);
+                File.WriteAllLines(OnPentaTxt, lines);
+            }
+            if (!File.Exists(OnAllyKillTxt))
+            {
+                var newfile = File.Create(OnAllyKillTxt);
+                newfile.Close();
+            }
+            if (!File.Exists(OnAllyDoubleTxt))
+            {
+                var newfile = File.Create(OnAllyDoubleTxt);
+                newfile.Close();
+            }
+            if (!File.Exists(OnAllyTripleTxt))
+            {
+                var newfile = File.Create(OnAllyTripleTxt);
+                newfile.Close();
+            }
+            if (!File.Exists(OnAllyQuadraTxt))
+            {
+                var newfile = File.Create(OnAllyQuadraTxt);
+                newfile.Close();
+            }
+            if (!File.Exists(OnAllyPentaTxt))
+            {
+                var newfile = File.Create(OnAllyPentaTxt);
+                newfile.Close();
+            }
+            if (!File.Exists(OnEnemyKillTxt))
+            {
+                var newfile = File.Create(OnEnemyKillTxt);
+                newfile.Close();
+            }
+            if (!File.Exists(OnEnemyDoubleTxt))
+            {
+                var newfile = File.Create(OnEnemyDoubleTxt);
+                newfile.Close();
+            }
+            if (!File.Exists(OnEnemyTripleTxt))
+            {
+                var newfile = File.Create(OnEnemyTripleTxt);
+                newfile.Close();
+            }
+            if (!File.Exists(OnEnemyQuadraTxt))
+            {
+                var newfile = File.Create(OnEnemyQuadraTxt);
+                newfile.Close();
+            }
+            if (!File.Exists(OnEnemyPentaTxt))
+            {
+                var newfile = File.Create(OnEnemyPentaTxt);
+                newfile.Close();
+            }
+        }
+    }
+}
--- NiceGuySharp/NiceGuySharp/Program.cs
+++ NiceGuySharp/NiceGuySharp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,32 @@ namespace NiceGuySharp
 {
     public class Program
     {
-        public static Menu menu;
+        public static Menu Menu;
+        public static Random Rand = new Random(Environment.TickCount);
+        public static Obj_AI_Hero MyHero = ObjectManager.Player;
+        public static List<Obj_AI_Hero> AllHeroes;
+        public static List<Obj_AI_Hero> AllyHeroes;
+        public static List<Obj_AI_Hero> EnemyHeroes;
+        public static int Deaths,
+            Kills,
+            Doubles,
+            Triples,
+            Quadras,
+            Pentas,
+            AllyKills,
+            AllyDeaths,
+            AllyDoubles,
+            AllyQuadras,
+            AllyPentas,
+            EnemyKills,
+            EnemyDeaths,
+            EnemyDoubles,
+            EnemyTriples,
+            EnemyQuadras,
+            EnemyPentas = 0;
+
+        public static int LastSentMessage;
+        public static int MinTimeBeforeNewMessage = Rand.Next(5000, 60000);
         public static void Main(string[] args)
         {
             CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
@@ -19,17 +45,135 @@ namespace NiceGuySharp
         public static void Game_OnGameLoad(EventArgs args)
         {
 
-            menu = new Menu("Nice Guy Sharp", "niceguy", true);
-            menu.AddItem(new MenuItem("enabled", "Enable").SetValue(true));
+            Menu = new Menu("Nice Guy Sharp", "niceguy", true);
+            Menu.AddItem(new MenuItem("enabled", "Enable").SetValue(true));
             
-            menu.AddToMainMenu();
-            Game.PrintChat("NICE GUY SHARP SUCCESFULLY LOADED.");
+            Menu.AddToMainMenu();
+            FileHandler.DoChecks();
+            AllHeroes = ObjectManager.Get<Obj_AI_Hero>().ToList();
+            AllyHeroes = AllHeroes.FindAll(hero => hero.IsAlly).ToList();
+            EnemyHeroes = AllHeroes.FindAll(hero => !hero.IsAlly).ToList();
+            Game.PrintChat("NICE-GUY SHARP SUCCESFULLY LOADED.");
+            
+            string[] onGameStart = File.ReadAllLines(FileHandler.OnGameStartTxt);
+            int randMessage = Rand.Next(onGameStart.Count());
+            if (onGameStart != null && onGameStart.Length > 0)
+            {
+                Game.Say(onGameStart[randMessage]);
+            }
 
             Game.OnGameUpdate += Game_OnGameUpdate;
         }
         public static void Game_OnGameUpdate(EventArgs args)
         {
-		
+            if (Menu.Item("enabled").GetValue<bool>())
+            {
+                if (Environment.TickCount - LastSentMessage > MinTimeBeforeNewMessage)
+                {
+
+                    if (RealAllyPentas() > AllyPentas)
+                    {
+                        string[] onAllyPenta = File.ReadAllLines(FileHandler.OnAllyPentaTxt);
+                        int randMessage = Rand.Next(onAllyPenta.Count());
+                        if (onAllyPenta != null && onAllyPenta.Length > 0)
+                        {
+                            Game.Say(onAllyPenta[randMessage]);
+                        }
+                        AllyPentas = RealAllyPentas();
+                    }
+                    if (RealEnemyPentas() > EnemyPentas)
+                    {
+                        string[] onEnemyPenta = File.ReadAllLines(FileHandler.OnEnemyPentaTxt);
+                        int randMessage = Rand.Next(onEnemyPenta.Count());
+                        if (onEnemyPenta != null && onEnemyPenta.Length > 0)
+                        {
+                            Game.Say(onEnemyPenta[randMessage]);
+                        }
+                        EnemyPentas = RealEnemyPentas();
+                    }
+
+                    if (MyHero.Deaths > Deaths)
+                    {
+                        string[] onDeath = File.ReadAllLines(FileHandler.OnDeathTxt);
+                        int randMessage = Rand.Next(onDeath.Count());
+                        if (onDeath != null && onDeath.Length > 0)
+                        {
+                            Game.Say(onDeath[randMessage]);
+                        }
+                        Deaths = MyHero.Deaths;
+                    }
+                    if (MyHero.DoubleKills > Doubles)
+                    {
+                        string[] onDouble = File.ReadAllLines(FileHandler.OnDoubleTxt);
+                        int randMessage = Rand.Next(onDouble.Count());
+                        if (onDouble != null && onDouble.Length > 0)
+                        {
+                            Game.Say(onDouble[randMessage]);
+                        }
+                        Doubles = MyHero.DoubleKills;
+                    }
+                    if (MyHero.TripleKills > Triples)
+                    {
+                        string[] onTriple = File.ReadAllLines(FileHandler.OnTripleTxt);
+                        int randMessage = Rand.Next(onTriple.Count());
+                        if (onTriple != null && onTriple.Length > 0)
+                        {
+                            Game.Say(onTriple[randMessage]);
+                        }
+                        Triples = MyHero.TripleKills;
+                    }
+                    if (MyHero.QuadraKills > Quadras)
+                    {
+                        string[] onQuadra = File.ReadAllLines(FileHandler.OnQuadraTxt);
+                        int randMessage = Rand.Next(onQuadra.Count());
+                        if (onQuadra != null && onQuadra.Length > 0)
+                        {
+                            Game.Say(onQuadra[randMessage]);
+                        }
+                        Quadras = MyHero.QuadraKills;
+                    }
+                    if (MyHero.PentaKills > Pentas)
+                    {
+                        string[] onPenta = File.ReadAllLines(FileHandler.OnPentaTxt);
+                        int randMessage = Rand.Next(onPenta.Count());
+                        if (onPenta != null && onPenta.Length > 0)
+                        {
+                            Game.Say(onPenta[randMessage]);
+                        }
+                        Pentas = MyHero.PentaKills;
+                    }
+                    if (MyHero.ChampionsKilled > Kills)
+                    {
+                        string[] onKill = File.ReadAllLines(FileHandler.OnKillTxt);
+                        int randMessage = Rand.Next(onKill.Count());
+                        if (onKill != null && onKill.Length > 0)
+                        {
+                            Game.Say(onKill[randMessage]);
+                        }
+                        Kills = MyHero.ChampionsKilled;
+                    }
+                }
+            }
+
+        }
+
+        public static int RealAllyPentas()
+        {
+            int pentakills = 0;
+            foreach (var hero in AllyHeroes)
+            {
+                pentakills += hero.PentaKills;
+            }
+            return pentakills;
+        }
+        public static int RealEnemyPentas()
+        {
+            int pentakills = 0;
+            foreach (var hero in EnemyHeroes)
+            {
+                pentakills += hero.PentaKills;
+            }
+            return pentakills;
         }
     }
 }

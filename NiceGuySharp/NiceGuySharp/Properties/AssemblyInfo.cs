8710f3389cf850491391ab0bd7d59e2e251cf3d6
imsosharp <imsosharp@mail.com>
2015-01-17 16:46:12 +0200

    versioning

--- NiceGuySharp/NiceGuySharp/Properties/AssemblyInfo.cs
+++ NiceGuySharp/NiceGuySharp/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("QuickSimpleYetSoSharpPositionRevealer")]
+[assembly: AssemblyTitle("NiceGuySharp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("QuickSimpleYetSoSharpPositionRevealer")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyCompany("LeagueSharp")]
+[assembly: AssemblyProduct("NiceGuySharp")]
+[assembly: AssemblyCopyright("Copyright © LeagueSharp 2014-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("5.1.0.67")]
+[assembly: AssemblyFileVersion("5.1.0.67")]

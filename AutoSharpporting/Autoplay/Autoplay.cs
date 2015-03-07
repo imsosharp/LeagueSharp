000c572310ba25ecf41c94d948e236fc7d3c8155
imsosharp <imsosharp@mail.com>
2015-03-07 21:52:42 +0200

    fixed autolevel #bigsorry

--- AutoSharpporting/Autoplay/Autoplay.cs
+++ AutoSharpporting/Autoplay/Autoplay.cs
@@ -112,8 +112,8 @@ namespace Support
                     _safe = Blue;
                 }
             }
-            AutoLevel levelUpSpells = new AutoLevel(TreesAutoLevel.GetSequence());
-            AutoLevel.Enabled(true);
+            new AutoLevel(TreesAutoLevel.GetSequence());
+            AutoLevel.Enable();
             FileHandler.DoChecks();
             MetaHandler.LoadObjects();
             
